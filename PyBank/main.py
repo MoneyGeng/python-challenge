763128216a73f2f3745204dda79f181e146d73ce
MoneyGeng <120427024+MoneyGeng@users.noreply.github.com>
2023-01-17 22:19:09 -0500

    finished project

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,4 +1,84 @@
-# importing
+# Importing dependencies
 import os
 import csv
 
+# Set path to csv file
+csv_path = 'C:\\Users\\John\\Desktop\\Data Analytics Bootcamp\\UTOR-VIRT-DATA-PT-12-2022-U-LOLC-main\\03-Python\\Challenge\\python-challenge\\PyBank\\Resource\\budget_data.csv'
+
+# Creating empty lists
+months_list = []
+amounts_list = []
+monthly_change_list = []
+
+# Declaring variables
+total_months = 0 
+total_amounts = 0
+average_change = 0
+greatest__increase_value = 0
+greatest__decrease_value = 0
+greatest__increase_month = ''
+greatest__decrease_month = ''
+
+# Set output file path
+output_path = "C:\\Users\\John\\Desktop\\Data Analytics Bootcamp\\UTOR-VIRT-DATA-PT-12-2022-U-LOLC-main\\03-Python\\Challenge\\python-challenge\\PyBank\Analysis\\Financial_Analysis_Summary.txt"
+
+# Reading csv file
+with open(csv_path) as csv_file:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csv_reader = csv.reader(csv_file, delimiter=',')
+
+    # Skipping header row
+    next(csv_reader)
+
+    # iterate through rows in the csv file
+    for row in csv_reader:
+
+        # Appending values of each row into corresponding lists
+        months_list.append(row[0])
+        amounts_list.append(int(row[1]))
+
+    # Set variables to hold number of months and total profit/losses
+    total_months = len(months_list)
+    total_amounts = sum(amounts_list)
+
+    # Iterate through amount list to get mohtly change in amounts
+    for i in range(len(amounts_list) - 1):
+
+        # Taking the difference between two months and appending to monthly_change_list 
+        monthly_change_list.append(amounts_list[i + 1] - amounts_list[i])
+
+    # Calculating average changes in profit and losses over entire period and storing in variable
+    average_change = sum(monthly_change_list)/len(monthly_change_list)
+
+    # Storing min and max of monthly change list
+    greatest__increase_value = max(monthly_change_list)
+    greatest__decrease_value = min(monthly_change_list)
+
+    # Obtaining correlating min and max months from months list by using index of min and max amounts
+    # Adding 1 to index as associated month with change is the month after
+    greatest__increase_month = months_list[monthly_change_list.index(max(monthly_change_list)) + 1]
+    greatest__decrease_month = months_list[monthly_change_list.index(min(monthly_change_list)) + 1]
+
+# Printing statements
+print("Financial Analysis")
+print("----------------------------")
+print(f"Total Months: {total_months}")
+print(f"Total: ${total_amounts}")
+print(f"Average Change: ${round(average_change,2)}")
+print(f"Greatest Increase in Profits: {greatest__increase_month} (${greatest__increase_value})")
+print(f"Greatest Decrease in Profits: {greatest__decrease_month} (${greatest__decrease_value})")
+
+# Outputting results
+with open(output_path,"w") as file:
+
+    # Writing statements to text file
+    file.write("Financial Analysis\n")
+    file.write("----------------------------\n")
+    file.write(f"Total Months: {total_months}\n")
+    file.write(f"Total: ${total_amounts}\n")
+    file.write(f"Average Change: ${round(average_change,2)}\n")
+    file.write(f"Greatest Increase in Profits: {greatest__increase_month} (${greatest__increase_value})\n")
+    file.write(f"Greatest Decrease in Profits: {greatest__decrease_month} (${greatest__decrease_value})\n")
+
+    
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,3 +1,83 @@
-# importing
+# Importing dependencies
 import os
 import csv
+
+# Set path to csv file
+csv_path = "Challenge\\python-challenge\\PyPoll\\Resource\\election_data.csv"
+
+# Set Output path
+output_path = "Challenge\\python-challenge\\PyPoll\\Analysis\\Election_Results.txt"
+
+# Creating empty candidate list
+candidate_list = []
+
+# Creating dictionary for candidates and their votes
+candidate_votes_dict = {} 
+
+# Declaring variables
+total_votes = 0
+
+# Reading csv file
+with open(csv_path) as csv_file:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csv_reader = csv.reader(csv_file, delimiter=',')
+
+    # Skipping header row
+    next(csv_reader)
+
+    # Iterate through rows in csv file
+    for row in csv_reader:
+        
+        # Tabulating total number of votes
+        total_votes += 1
+
+        # Appending unique values in candiate column to create candidate list
+        if row[2] not in candidate_list:
+            candidate_list.append(row[2])
+
+        # Iterating though elements of Candidate list
+        for candidate in candidate_list:
+
+            # Adding list elements to candidate dictionary as keys
+            if candidate not in candidate_votes_dict:
+                candidate_votes_dict[candidate] = 0
+            
+            # Tabulating votes to each candidate
+            if row[2] == candidate:
+                candidate_votes_dict[candidate] += 1
+
+# Return the winner by using max function of dictionary
+winner = max(candidate_votes_dict, key=candidate_votes_dict.get)
+
+# Calculating percentage of results
+stockham_percent = (candidate_votes_dict['Charles Casper Stockham']/total_votes)*100
+degette_percent = (candidate_votes_dict['Diana DeGette']/total_votes)*100
+doane_percent = (candidate_votes_dict['Raymon Anthony Doane']/total_votes)*100
+
+# Print the summary table
+print(f"Election Results")
+print(f"----------------------------")
+print(f"Total Votes: {total_votes}")
+print(f"----------------------------")
+print(f"Charles Casper Stockham: {round(stockham_percent,3)}% ({candidate_votes_dict['Charles Casper Stockham']})")
+print(f"Diana DeGette: {round(degette_percent,3)}% ({candidate_votes_dict['Diana DeGette']})")
+print(f"Raymon Anthony Doane: {round(doane_percent,3)}% ({candidate_votes_dict['Raymon Anthony Doane']})")
+print(f"----------------------------")
+print(f"Winner: {winner}")
+print(f"----------------------------")
+
+# Outputting results
+with open(output_path,"w") as file:
+
+    # Writing statements in output file
+    file.write(f"Election Results\n")
+    file.write(f"----------------------------\n")
+    file.write(f"Total Votes: {total_votes}\n")
+    file.write(f"----------------------------\n")
+    file.write(f"Charles Casper Stockham: {round(stockham_percent,3)}% ({candidate_votes_dict['Charles Casper Stockham']})\n")
+    file.write(f"Diana DeGette: {round(degette_percent,3)}% ({candidate_votes_dict['Diana DeGette']})\n")
+    file.write(f"Raymon Anthony Doane: {round(doane_percent,3)}% ({candidate_votes_dict['Raymon Anthony Doane']})\n")
+    file.write(f"----------------------------\n")
+    file.write(f"Winner: {winner}\n")
+    file.write(f"----------------------------\n")
